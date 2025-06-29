b974900e8ff1005a7f84e79be5d40902fba55ab0
rohitsharma2-3 <rohit.sharma2k2k@gmail.com>
2025-06-29 12:21:10 +0530

    frontend setUp

--- backend/Models/UserModel.js
+++ backend/Models/UserModel.js
@@ -1,5 +1,5 @@
-const { model} = require('mongoose')
-const userSchema = require('../Schema/UserSchema')
+const { model } = require('mongoose')
+const { userSchema } = require('../Schema/UserSchema')
 
 const User = new model('User', userSchema)
 
--- frontend/src/Components/SignUp.jsx
+++ frontend/src/Components/SignUp.jsx
@@ -2,16 +2,65 @@ import React, { useState } from 'react';
 
 const SignUp = () => {
   const [login, setLogin] = useState('Login')
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  })
+
+  const inputHandler = (e) => {
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+  }
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+    console.log(formData)
+    setFormData({
+      name: '',
+      email: '',
+      password: '',
+    })
+  }
+
+  const signUpFun = async () => {
+    let respData;
+    await fetch('http://localhost:4000/verifiedvilla/signup', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    }).then((res) => res.json())
+      .then((data) => respData = data)
+
+    if (respData.success) {
+      localStorage.setItem('auth-token', respData.token)
+      localStorage.setItem('username', formData.username)
+      window.location.replace('/')
+    } else {
+      alert(respData.error)
+    }
+  }
+
+  const loginFun = async () => {
+   
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center ">
       <div className="bg-white p-8 rounded-lg  w-4/4 md:w-3/4 xl:w-2/4 ">
         <h2 className="text-2xl font-semibold mb-6 text-center">Create Account</h2>
-        <form>
+        <form onSubmit={onSubmitHandler}>
           {login === 'SignUp' ? <div className="mb-4">
             <label htmlFor="name" className="block text-gray-700 font-medium mb-2">
               Full Name
             </label>
             <input
+              name='name'
+              value={formData.name}
+              onChange={inputHandler}
               type="text"
               id="name"
               placeholder="John Doe"
@@ -24,6 +73,9 @@ const SignUp = () => {
               Email
             </label>
             <input
+              name='email'
+              value={formData.email}
+              onChange={inputHandler}
               type="email"
               id="email"
               placeholder="you@example.com"
@@ -36,6 +88,9 @@ const SignUp = () => {
               Password
             </label>
             <input
+              name='password'
+              value={formData.password}
+              onChange={inputHandler}
               type="password"
               id="password"
               placeholder="••••••••"
@@ -49,7 +104,8 @@ const SignUp = () => {
 
 
           {/* Button */}
-          <button className="group relative inline-block w-full mt-3 text-sm font-medium text-indigo-600 focus:ring-3 focus:outline-hidden">
+          <button className="group relative inline-block w-full mt-3 text-sm font-medium text-indigo-600 focus:ring-3 focus:outline-hidden"
+            onClick={login === "SignUp" ? signUpFun : loginFun}>
             <span className="absolute inset-0 translate-x-0.5 translate-y-0.5 bg-indigo-600 transition-transform group-hover:translate-x-0 group-hover:translate-y-0"></span>
 
             <span className="relative block border border-current bg-white px-8 py-3"> {login === "Login" ? 'Login' : 'SignUp'} </span>
